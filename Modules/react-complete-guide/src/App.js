efd1c58396e1c6e5ded2f428e2bf0eb7322e07ce
Rachael <rachael@bodigiti.com>
2020-08-31 13:55:39 -0400

    Academind Module 3 'Working with Lists and Conditionals' right before going into Outputting Lists

--- Modules/react-complete-guide/src/App.js
+++ Modules/react-complete-guide/src/App.js
@@ -70,8 +70,83 @@ class App extends Component {
 
 export default App;
 
+/********************** Reference Code Below **********************/
 
-//Alternative way to Toggle Div to Show/Hide all Person components; considered less elegant
+/* Render content conditionally. In this case, we want to 
+Toggle the Div to Show/Hide all Person components */
+
+// class App extends Component {
+
+// 	state = {
+// 		persons: [
+// 			{ name: 'Adam', age: 800},
+//       { name: 'Abel', age: 30},
+//       { name: 'Abinadab', age: 250}
+// 		]
+// 	}
+// 	/* Method to make the Switch Name button below cause a change */
+// 	switchAgeHandler = () => {
+// 		this.setState({
+// 			persons: [
+// 				{ name: 'Adam', age: 900},
+//         { name: 'Abel', age: 35},
+//         { name: 'Abinadab', age: 200}
+// 			],
+// 			showPersons: false
+// 		});
+// 	}
+// 	/* Method to Show/Hide all Person components; based on Boolean, true or false */
+// 	togglePersonHandler = () => {
+// 		const doesShow = this.state.showPersons;
+// 		this.setState({showPersons: !doesShow});
+// 	}
+
+
+// 	render() {
+// 		const styling = {
+// 			backgroundColor: 'lightblue',
+// 			font: 'inherit',
+// 			padding: '8px',
+// 			margin: '.5rem',
+// 			cursor: 'pointer'
+// 		};
+
+// 		let persons = null; 
+// 		if (this.state.showPersons) {
+// 			persons = (
+// 				<div >{/* Toggled Div to Show/Hide all Person components*/} 
+// 					<Person 
+// 						name={this.state.persons[0].name}
+// 						age={this.state.persons[0].age}></Person>
+// 					<Person
+// 						name={this.state.persons[1].name}
+// 						age={this.state.persons[1].age}></Person>
+// 					<Person 
+// 						name={this.state.persons[2].name}
+// 						age={this.state.persons[2].age}></Person>
+// 				</div>/* End Toggled Div to Show/Hide all Person components*/
+// 			);
+// 		}
+
+//     return (
+//       <div className='App'>
+// 				<button 
+// 				style={styling}
+// 				onClick={this.togglePersonHandler}>Show/Hide Persons</button>
+// 				{persons}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+/*Alternative way to render content conditionally. In this case, we want to 
+Toggle the Div to Show/Hide all Person components; considered less elegant than the method that
+precedes
+*/
+
 // class App extends Component {
 
 // 	state = {
@@ -134,3 +209,5 @@ export default App;
 // }
 
 // export default App;
+
+
