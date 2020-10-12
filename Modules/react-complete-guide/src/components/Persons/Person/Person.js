5d4ff1126302fb88cdbf577f24a26a27f44092ef
Rachael <rachael@bodigiti.com>
2020-10-12 13:18:35 -0400

    Module 7 lesson re: Adjacent JSX Elements using Aux

--- Modules/react-complete-guide/src/components/Persons/Person/Person.js
+++ Modules/react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,20 +1,22 @@
 import React, {Component} from 'react';
-import classes from './Person.css';
+// import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component { 
 	render () {
 		console.log('[Person.js] rendering...');
 		return (
-			<div className={classes.Person}>
-				<p onClick={this.props.click}>I'm {this.props.name}. I'm {this.props.age} years old.<br>
-					</br>My username is <b>{this.props.username}</b></p>
-				<p>{this.props.children}</p>
-				<input
-					type="text"
-					onChange={this.props.change}
-					value={this.props.username}></input>
-			</div>
-		);
+		<Aux>
+			<p onClick={this.props.click}>
+			I'm {this.props.name}. I'm {this.props.age} years old.<br></br>
+			My username is <b>{this.props.username}</b></p>,
+			<p>{this.props.children}</p>,
+			<input
+			type="text"
+			onChange={this.props.change}
+			value={this.props.username}>
+			</input>
+		</Aux>);
 	}
 };
 
