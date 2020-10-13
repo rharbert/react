772808c46a53a55a4028372dfb09edc8b888b653
Rachael <rachael@bodigiti.com>
2020-10-13 17:23:24 -0400

    Module 7 Using PropTypes lesson completed; in Person.js file

--- Modules/react-complete-guide/src/components/Persons/Person/Person.js
+++ Modules/react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.css';
 import Aux from '../../../hoc/Auxiliary';
 import withClass from '../../../hoc/withClass';
@@ -23,6 +24,14 @@ class Person extends Component {
 			</Aux>
 		);
 	}
+}
+
+Person.propTypes = {
+click: PropTypes.func, 
+name: PropTypes.string, 
+age: PropTypes.number, 
+username: PropTypes.string,
+changed: PropTypes.func
 };
 
 export default withClass(Person, classes.Person);  
